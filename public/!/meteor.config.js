3ea954e09023bb86a16b0a235f9edbebd3d43f9e
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:41:46 -0400

    Update meteor.config.js

--- public/!/meteor.config.js
+++ public/!/meteor.config.js
@@ -1,39 +1,8 @@
-export const config = {
-  prefix: "/route/",
-  codec: self.$meteor_codecs.xor,
-  debug: true,
+// meteor.config.js
+// Proxy configuration
 
-  plugins: [
-    {
-      name: "exampleplugin",
-      filter: /https:\/\/example.com*/g,
-      async inject(ctx) {
-        await ctx.injectHTML(`
-          <meta name="meteor" content="meteor - epic proccy">
-          <script x-inject="true" src="data:application/javascript,console.log('pneis')"></script>
-          `)
-      },
-      async onRequest(request) {
-        request.headers.set("X-Proxy", "Meteor")
-        return request
-      },
-      handleClient(window) {
-        window.console.log("Meteor is running on the client!")
-        const ws = new WebSocket("wss://echo.websocket.org/")
-        ws.addEventListener("message", e => {
-          console.log(e.data)
-        })
-      }
-    }
-  ],
-
-  files: {
-    client: "/!/meteor.client.js",
-    worker: "/!/meteor.worker.js",
-    bundle: "/!/meteor.bundle.js",
-    codecs: "/!/meteor.codecs.js",
-    config: "/!/meteor.config.js"
-  }
-}
-
-self.$meteor_config = config
+Meteor.Config = {
+    proxyUrl: '/proxy', // example path in your public folder
+    useWorker: true,
+    cacheEnabled: true
+};
