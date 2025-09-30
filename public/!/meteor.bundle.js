af9e2d3f5044bda7b9ae8a27bcc2b9649ec88623
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:40:51 -0400

    Update meteor.bundle.js

--- public/!/meteor.bundle.js
+++ public/!/meteor.bundle.js
@@ -1,1 +1,9 @@
+// meteor.bundle.js
+// Main Meteor bundle entry point
 
+import './meteor.client.js';
+import './meteor.codecs.js';
+import './meteor.config.js';
+import './meteor.worker.js';
+
+console.log("Meteor bundle loaded");
