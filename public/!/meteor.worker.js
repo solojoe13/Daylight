e882d2c39f5c5fb413f867953fa80e026217242e
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:50:17 -0400

    Update meteor.worker.js

--- public/!/meteor.worker.js
+++ public/!/meteor.worker.js
@@ -1,8 +1,6 @@
 // meteor.worker.js
-
 self.onmessage = function(e) {
     console.log("Worker received:", e.data);
-    // Example: decode data
     const decoded = Meteor.Codecs.decode(e.data);
     self.postMessage(decoded);
 };
