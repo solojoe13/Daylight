5d42119e81d6337200385d6d2ee80494a51e5ce9
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:53:11 -0400

    Update meteor.worker.js

--- public/!/meteor.worker.js
+++ public/!/meteor.worker.js
@@ -1,6 +1,15 @@
 // meteor.worker.js
-self.onmessage = function(e) {
-    console.log("Worker received:", e.data);
-    const decoded = Meteor.Codecs.decode(e.data);
-    self.postMessage(decoded);
+self.onmessage = async function(e) {
+    const data = Meteor.Codecs.decode(e.data);
+
+    if (!data) return;
+
+    try {
+        const res = await fetch(data.url, data.options);
+        const json = await res.json();
+        self.postMessage(Meteor.Codecs.encode(json));
+    } catch (err) {
+        console.error("Worker fetch error:", err);
+        self.postMessage(Meteor.Codecs.encode({ error: err.message }));
+    }
 };
