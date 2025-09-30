a1716e73baa8f2135dc25d20ec194ef15225d7e0
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:25:11 -0400

    Update meteor.config.js

--- public/!/meteor.config.js
+++ public/!/meteor.config.js
@@ -1,14 +1,39 @@
-const config = {
-    prefix: '/!/elysium/',
-    codec: $meteor_codecs.xor,
-    debug: true,
-    files: {
-      client: '/!/meteor.client.js',
-      worker: '/!/meteor.worker.js',
-      bundle: '/!/meteor.bundle.js',
-      codecs: '/!/meteor.codecs.js',
-      config: '/!/meteor.config.js'
+export const config: Config = {
+  prefix: '/route/',
+  codec: self.$meteor_codecs.xor,
+  debug: true,
+
+  plugins: [
+    {
+      name: 'exampleplugin',
+      filter: /https:\/\/example.com*/g,
+      async inject(ctx) {
+        await ctx.injectHTML(`
+          <meta name="meteor" content="meteor - epic proccy">
+          <script x-inject="true" src="data:application/javascript,console.log('pneis')"></script>
+          `)
+      },
+      async onRequest(request) {
+        request.headers.set('X-Proxy', 'Meteor')
+        return request
+      },
+      handleClient(window) {
+        window.console.log('Meteor is running on the client!')
+        const ws = new WebSocket('wss://echo.websocket.org/')
+        ws.addEventListener('message', (e) => {
+          console.log(e.data)
+        })
+      }
     }
+  ],
+
+  files: {
+    client: '/meteor/meteor.client.js',
+    worker: '/meteor/meteor.worker.js',
+    bundle: '/meteor/meteor.bundle.js',
+    codecs: '/meteor/meteor.codecs.js',
+    config: '/meteor/meteor.config.js'
   }
-  
-  self.__meteor$config = config
+}
+
+self.$meteor_config = config
