c61a9c2fc3ecda427bd09624e2414573daec7fb7
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:52:52 -0400

    Update sw.js

--- public/!/sw.js
+++ public/!/sw.js
@@ -6,7 +6,16 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
+
+    // Intercept proxy requests
     if (url.pathname.startsWith('/proxy')) {
-        event.respondWith(fetch(event.request));
+        event.respondWith(
+            fetch(event.request)
+                .then(res => res)
+                .catch(err => new Response(JSON.stringify({ error: err.message }), {
+                    status: 500,
+                    headers: { 'Content-Type': 'application/json' }
+                }))
+        );
     }
 });
