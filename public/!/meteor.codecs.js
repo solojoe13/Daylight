5d52454228c70fcfe1e78fb4eed051675b91584f
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 12:41:27 -0400

    Update meteor.codecs.js

--- public/!/meteor.codecs.js
+++ public/!/meteor.codecs.js
@@ -1,1 +1,16 @@
+// meteor.codecs.js
+// Encode/decode messages
 
+Meteor.Codecs = {
+    encode: function(obj) {
+        return JSON.stringify(obj);
+    },
+    decode: function(str) {
+        try {
+            return JSON.parse(str);
+        } catch (e) {
+            console.error("Decoding error:", e);
+            return null;
+        }
+    }
+};
